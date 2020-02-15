93f47e2c414d6992d6b3464d1b47e32b26a40cab
Michelle To <tohoangyennhi@gmail.com>
2020-02-14 16:17:33 -0800

    wk 6 excer.

--- chapter-16-exercises/exercise-2/exercise.R
+++ chapter-16-exercises/exercise-2/exercise.R
@@ -14,6 +14,8 @@ library("ggplot2")
 # clarity. You should see a _stacked_ bar chart.
 
 
+
+
 # Draw the same chart again, but with each element positioned to "fill" the y axis
 
 
@@ -35,29 +37,40 @@ library("ggplot2")
 
 # Draw a "boxplot" (with `geom_boxplot`) for the diamond's price (y) by color (x)
 
+ggplot(data = diamonds_samples) +
+  geom_boxplot(mapping = aes ( x = color, y =price))
 
 # This has a lot of outliers, making it harder to read. To fix this, draw the 
 # same plot but with a _logarithmic_ scale for the y axis.
 
-
+ggplot(data = diamonds_samples) +
+  geom_boxplot(mapping = aes ( x = color, y =price)) +
+    scale_y_log10()
 # For another version, draw the same plot but with `violin` geometry instead of 
 # `boxplot` geometry!
 # How does the logarithmic scale change the data presentation?
 
-
+ggplot(data = diamonds_samples) +
+  geom_violin(mapping = aes ( x = color, y =price)) +
+  scale_y_log10()
 # Another interesting plot: draw a plot of the diamonds price (y) by carat (x), 
 # using a heatmap of 2d bins (geom_bin2d)
 # What happens when you make the x and y channels scale logarithmically?
 
-
+ggplot(data = diamonds_samples) +
+  geom_bin2d(mapping = aes(x = carat, y = price)) +
+  scale_x_log10() +
+  scale_y_log10()
 # Draw a scatter plot for the diamonds price (y) by carat (x). Color each point
 # by the clarity (Remember, this will take a while. Use a sample of the diamonds 
 # for faster results)
-
+ggplot(data = diamonds_samples) +
+  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
+  scale_color_brewer(palette = "Purples", direction = -1)
 
 # Change the color of the previous plot using a ColorBrewer scale of your choice. 
 # What looks nice?
-
+#  scale_color_brewer(palette = "Purples")
 
 
 ## Coordinate Systems
@@ -81,6 +94,9 @@ library("ggplot2")
 # _facets_ based on the diamond's `color`
 
 
+ggplot(data = diamonds_samples) +
+  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
+  facet_wrap(~clarity)
 
 ## Saving Plots
 
